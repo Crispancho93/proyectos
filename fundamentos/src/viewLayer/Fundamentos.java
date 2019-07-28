9c2f1d49cc6b49c5eb9b3269ceefefe256c92f25
Crispancho93 <34691721+Crispancho93@users.noreply.github.com>
2019-07-28 14:38:42 -0500

    test netbeans

---
+++ fundamentos/src/viewLayer/Fundamentos.java
@@ -0,0 +1,16 @@
+package viewLayer;
+
+/**
+ *
+ * @author carboleda
+ */
+public class Fundamentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
