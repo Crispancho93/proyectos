0284ef39e9df7f9f67232e8a66a3437b449e4867
Crispancho93 <34691721+Crispancho93@users.noreply.github.com>
2019-08-04 15:45:02 -0500

    frmEmpleado

---
+++ comercial/src/conexion/Conexion.java
@@ -0,0 +1,48 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Crispancho
+ */
+public class Conexion {
+    
+    private static Connection conn = null;
+    private static String user = "system";
+    private static String password = "Colombia.123";
+    private static String url = "jdbc:oracle:thin:@10.10.0.1:1521:xe";
+    
+    public static Connection getConnection(){
+        
+        try {
+            
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection(url, user, password);
+            conn.setAutoCommit(false);
+            System.out.println("Conexión establecida");
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("No se puede conectar " + e.getMessage());
+        }
+        
+        return conn;
+    }
+    
+    public void desconectar(){
+        
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("No se puede desconectar");
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        Conexion con = new Conexion();
+        con.getConnection();
+    }
+}
--- comercial/src/entity/Empleado.java
+++ comercial/src/entity/Empleado.java
@@ -11,4 +11,92 @@ package entity;
  */
 public class Empleado {
     
+    private int idEmpleado;
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String estadoCivil;
+    private String genero;
+    private int edad;
+    
+    public Empleado(){
+        
+    }
+    
+    public Empleado(int idEmpleado,String nombre, String apellido, String cedula, 
+            String estadoCivil, String genero, int edad){
+        
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.estadoCivil = estadoCivil;
+        this.genero = genero;
+        this.edad = edad;
+    }
+    
+    
+    public int getIdEmpleado(){
+        return idEmpleado;
+    }
+    
+    public void setIdEmpleado(int idEmpleado){
+        this.idEmpleado = idEmpleado;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getApellido(){
+        return apellido;
+    }
+    
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    
+    public String getCedula(){
+        return cedula;
+    }
+    
+    public void setCedula(String cedula){
+        this.cedula = cedula;
+    }
+    
+    public String getEstadoCivil(){
+        return estadoCivil;
+    }
+    
+    public void setEstadoCivil(String estadoCivil){
+        this.estadoCivil = estadoCivil;
+    }
+    
+    public String getGenero(){
+        return genero;
+    }
+
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "idEmpleado=" + idEmpleado + ", nombre=" + nombre + ", apellido=" + apellido + ", cedula=" + cedula + ", estadoCivil=" + estadoCivil + ", genero=" + genero + ", edad=" + edad + '}';
+    }
+    
+    
+    
 }
